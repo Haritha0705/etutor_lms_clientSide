2928225d27651de5dee48c8524de53eb7260f1fc
Haritha Wikramasinha <harithawikramasinha2003@gmail.com>
2025-09-17 23:31:17 +0530

    add new ui chages in heso section

--- client/components/public/Landing Page/sections/BottomBanner.section.tsx
+++ client/components/public/Landing Page/sections/BottomBanner.section.tsx
@@ -1,8 +1,30 @@
+import {Button} from "@/components/ui/button";
+
 const BottomBannerSection = () => {
     return (
-        <div>
-
-        </div>
+        <section className={'bg-gray-900 flex flex-row gap-5 items-center justify-between  py-15 w-full  px-58'}>
+            <div className={'text-white flex flex-col gap-10'}>
+                <h1 className={'text-4xl max-w-120  font-semibold'}>Start learning with 67.1k students around the world.</h1>
+                <div className={'flex gap-5'}>
+                    <Button className={'justify-center'}>Join the Family</Button>
+                    <Button className={'text-white justify-center'}>Browse all courses</Button>
+                </div>
+            </div>
+            <div className={'text-white flex gap-5'}>
+                <div className={'flex flex-col gap-5'}>
+                    <span className={'text-4xl font-semibold'}>6.3k</span>
+                    <p className={'text-sm text-gray-500'}>Online courses</p>
+                </div>
+                <div className={'flex flex-col gap-5'}>
+                    <span  className={'text-4xl font-semibold'}>26k</span>
+                    <p className={'text-sm text-gray-500'}>Certified Instructor</p>
+                </div>
+                <div className={'flex flex-col gap-5'}>
+                    <span  className={'text-4xl font-semibold'}>99.9%</span>
+                    <p className={'text-sm text-gray-500'}>Sucess Rate</p>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- client/components/public/Landing Page/sections/TrustedCompanies.section.tsx
+++ client/components/public/Landing Page/sections/TrustedCompanies.section.tsx
@@ -1,8 +1,30 @@
-const TrustedCompaniesSection = () => {
-    return (
-        <div>
+import Logo1 from '../../../../public/icons/Netfixsvg.svg'
+import Logo2 from '../../../../public/icons/Youtube.svg'
+import Logo3 from '../../../../public/icons/Google.svg'
+import Logo4 from '../../../../public/icons/Lenovo.svg'
+import Logo5 from '../../../../public/icons/Slack.svg'
+import Logo6 from '../../../../public/icons/Vesion.svg'
+import Logo7 from '../../../../public/icons/Lemark.svg'
+import Logo8 from '../../../../public/icons/Microsoft.svg'
+import Image from "next/image";
 
+const TrustedCompaniesSection = () => {
+    const companyLogos = [Logo1,Logo2,Logo3,Logo4,Logo5,Logo6,Logo7,Logo8]
+    const companyList: React.JSX.Element[] = companyLogos.map((logo,index) =>(
+        <div key={index} className={'shadow bg-white py-5 px-10'}>
+            <Image src={logo} alt={"company Logos"}/>
         </div>
+    ))
+    return (
+        <section className={'bg-white w-full pb-15 flex flex-row justify-between items-center pt-90 px-60'}>
+            <div className={'flex flex-col gap-5'}>
+                <h3 className={'font-semibold text-2xl'}>6.3k trusted companies</h3>
+                <p className={'max-w-xs text-gray-500'}>Nullam egestas tellus at enim ornare tristique. Class aptent taciti sociosqu ad litora torquent per conubia nostra.</p>
+            </div>
+            <div className={'grid grid-cols-4 gap-8'}>
+                {companyList}
+            </div>
+        </section>
     );
 };
 
