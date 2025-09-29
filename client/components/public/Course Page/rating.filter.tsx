ce10ee6f3f14d91e903d35518bfe9fdc43cbbd0a
Haritha Wikramasinha <harithawikramasinha2003@gmail.com>
2025-09-29 23:27:53 +0530

    crete rathigs filter

---
+++ client/components/public/Course Page/rating.filter.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {Category} from "@/types/course.types";
+import {RatingsFilter} from "@/enum/category.enum";
+
+type RatingProps = {
+    subCategories?: Category[];
+    setCount: React.Dispatch<React.SetStateAction<number>>;
+    selectedCategories: string[];
+    setSelectedCategories: React.Dispatch<React.SetStateAction<string[]>>;
+};
+
+const RatingFilter = ({}:RatingProps) => {
+    return (
+        <>
+            {RatingsFilter.map((rating, ratingIndex) => (
+                <div
+                    key={ratingIndex}
+                    className="flex items-center justify-between p-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
+                >
+                    <div
+                        className="flex items-center space-x-2 cursor-pointer"
+                    >
+                        <Label
+                            htmlFor={`rating-${ratingIndex}`}
+                            className={`flex items-center text-md pl-2 cursor-pointer 
+                            }`}
+                        >
+                            <Input
+                                type="checkbox"
+                                className="mr-2 w-4 h-4"
+                            />
+                            {rating.key}
+                        </Label>
+                    </div>
+                    <span className="text-sm">{rating.label}</span>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default RatingFilter;
